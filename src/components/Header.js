dad4d09446f99669632a91963076c70df2cb1d7e
dawnkelly09 <dawnkelly09@gmail.com>
2022-12-20 20:33:30 -0800

    initial commit for cash'd

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header>
+        <h1>Welcome to Cash'd</h1>
+        <h2>Cash’d is an online cannabis tasting journal which helps connoisseurs document and share their experiences with the cannabis community.</h2>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import logo from '/Users/dawnkelly/projects/cashd-app/src/assets/cashdLogo.png'
+
+const Navbar = () => {
+    return (
+        <div className='nav-container'>
+            <img className='nav-logo' src={logo} alt ="logo" />
+        </div>
+    )
+} 
+
+export default Navbar
