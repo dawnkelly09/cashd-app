f19cc3a24391cdc962cb150f9d258840779ede2e
dawnkelly09 <dawnkelly09@gmail.com>
2022-12-24 09:50:52 -0800

    styling, layout adjustments

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,16 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 
 const Header = () => {
+
+  const onClick = () => {
+    console.log('Click')
+    
+  }
   return (
     <header className='header'>
         <h1>Welcome to Cash'd</h1>
         <h2>Cash’d gives you the tools to find your ideal cannabis strain</h2>
-        <Button color='green' text='Suggest a Strain'/>   
+          
     </header>
   )
 }
