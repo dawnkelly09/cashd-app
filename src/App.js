ca70ff1573907291a54da2bd9403b243636f8a8a
dawnkelly09 <dawnkelly09@gmail.com>
2022-12-26 20:22:28 -0800

    fixing NodeS

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from './components/Header'
+import Navbar from './components/Navbar'
+function App() {
+  return (
+    <div className='container'>
+      <Navbar />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
